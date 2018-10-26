eed4835bf4e5eb53eba32b5f5608b5d964c87cec
MokkaCicc <velten.baptiste@gmail.com>
2018-10-26 09:54:20 +0200

    Rajout de printf pour plus de lisibilité

--- codeSource/main.c
+++ codeSource/main.c
@@ -15,11 +15,14 @@
 int main(int argc, char *argv[]) {
     //demande de deux nombres à lutilisateur
     int saisie1, saisie2;
+    printf("Calcul du PGCD de deux nombre\n");
+    printf("Nombre 1:");
     scanf("%d", &saisie1);
+    printf("Nombre 2:");
     scanf("%d", &saisie2);
 
     //affichage du résultat
-    printf("%d", gcd(saisie1, saisie2));
+    printf("\nLe résultat est: %d", gcd(saisie1, saisie2));
 
     //juste pour éviter la fermeture de la console
     scanf("%d", &saisie1);
