30550b5884d18a2de5e053fe304e5afbfb5cab57
Heli0os <44435709+Heli0os@users.noreply.github.com>
2018-10-26 10:04:25 +0200

    modif/ajout commentaires

--- codeSource/main.c
+++ codeSource/main.c
@@ -13,13 +13,13 @@
 
 //boucle principale
 int main(int argc, char *argv[]) {
-    //demande de deux nombres à lutilisateur
+    //demande de deux nombres à l'utilisateur
     int saisie1, saisie2;
     printf("Calcul du PGCD de deux nombre\n");
     printf("Nombre 1:");
-    scanf("%d", &saisie1);
+    scanf("%d", &saisie1); //demande à l'utilisateur le 1er nombre
     printf("Nombre 2:");
-    scanf("%d", &saisie2);
+    scanf("%d", &saisie2); //demande à l'utilisateur le second nombre
 
     //affichage du résultat
     printf("\nLe resultat est: %d", gcd(saisie1, saisie2));
